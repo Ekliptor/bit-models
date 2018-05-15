d8de951b39df3034be9e5a49682d30f705df4235
Ekliptor <Ekliptor1@yandex.com>
2018-05-15 09:05:11 +0700

    initial commit

---
+++ models.ts
@@ -0,0 +1,35 @@
+import {DatabaseObject} from "./models/base/DatabaseObject";
+import {AssetAction} from "./models/base/AssetAction";
+import * as Currency from "./models/base/Currency";
+
+import * as Candle from "./models/Candle";
+import * as Order from "./models/Order";
+import * as MarketOrder from "./models/MarketOrder";
+import * as LoanOrder from "./models/LoanOrder";
+import * as SystemMessage from "./models/SystemMessage";
+import * as Trade from "./models/Trade";
+import * as TradeHistory from "./models/TradeHistory";
+import * as BotTrade from "./models/BotTrade";
+import * as Ticker from "./models/Ticker";
+import * as TickerVolumeSpike from "./models/TickerVolumeSpike";
+import * as Funding from "./models/Funding";
+import * as conversation from "./models/conversation";
+import * as CoinMarketInfo from "./models/CoinMarketInfo";
+import * as user from "./models/user";
+import * as serverConfig from "./models/serverConfig";
+import * as Process from "./models/Process";
+
+import * as SocialAction from "./models/Social/SocialAction";
+import * as SocialPost from "./models/Social/SocialPost";
+import * as Tweet from "./models/Social/Tweet";
+import * as News from "./models/Social/News";
+import * as TrollShout from "./models/Social/TrollShout";
+
+export {
+    // base classes
+    DatabaseObject, AssetAction, Currency,
+
+    // main database models
+    Candle, Order, MarketOrder, LoanOrder, SystemMessage, Trade, TradeHistory, BotTrade, Ticker, TickerVolumeSpike, Funding, conversation, CoinMarketInfo, user, serverConfig,
+    Process, SocialAction, SocialPost, Tweet, News, TrollShout
+};
